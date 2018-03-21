8bf6bca80ea303b331b665c505846c044b8c9aa6
Michael Galloy <mgalloy@gmail.com>
2018-03-20 18:48:42 -0600

    More cleanup

--- scripts/hpss_gateway.sh
+++ scripts/hpss_gateway.sh
@@ -1,5 +1,15 @@
 #!/bin/sh
 
+# need both action and instrument arguments to work
+if [ $# -lt 2 ]; then
+  echo "usage: $(basename $0) action instrument"
+  echo
+  echo "positional arguments:"
+  echo "  action      action to perform: start, stop, or restart"
+  echo "  instrument  instrument to watch for: KCor or CoMP"
+  exit 1
+fi
+
 umask 0002
 
 # start and stop the HPSS gateway
@@ -11,19 +21,28 @@ BIN_DIR=$(dirname $0)
 ACTION=$1
 INSTRUMENT=$2
 
+
+kill_instrument () {
+  pkill -f "watch_hpss $1"
+}
+
+start_instrument () {
+  $BIN_DIR/watch_hpss $1
+}
+
 case "$ACTION" in
 
 start)
-  $BIN_DIR/watch_hpss $INSTRUMENT
+  start_instrument $INSTRUMENT
   ;;
 
 stop)
-  pkill -f "watch_hpss $INSTRUMENT"
+  kill_instrument $INSTRUMENT
   ;;
 
 restart)
-  pkill -f "watch_hpss $INSTRUMENT"
-  $BIN_DIR/watch_hpss $INSTRUMENT
+  kill_instrument $INSTRUMENT
+  start_instrument $INSTRUMENT
   ;;
 esac
 
