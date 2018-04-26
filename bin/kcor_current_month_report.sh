31204f483f54f04adf988aa7df180e1f23289336
Michael Galloy <mgalloy@gmail.com>
2018-04-26 11:15:53 -0600

    Run script with given Python

--- bin/kcor_current_month_report.sh
+++ bin/kcor_current_month_report.sh
@@ -4,4 +4,4 @@ PYTHON=/home/mgalloy/anaconda3/bin/python
 BIN_ROOT=$(dirname $0)
 DATA_ROOT=/hao/mahidata1/Data/KCor
 
-${BIN_ROOT}/kcor_report -d ${DATA_ROOT} $(date +"%Y %m")
+${PYTHON} ${BIN_ROOT}/kcor_report -d ${DATA_ROOT} $(date +"%Y %m")
