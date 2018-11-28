0bac40845188e57996c5f619fc6fc7021ad83812
Michael Galloy <mgalloy@gmail.com>
2018-11-27 17:22:20 -0700

    Sending HTML email for HPSS report

--- bin/hpss_report.sh
+++ bin/hpss_report.sh
@@ -2,10 +2,20 @@
 
 export PATH=/opt/local/hpss/bin:$PATH
 
+ROOT=/CORYDN
+
 PYTHON=/home/mgalloy/anaconda3/bin/python
 HPSS=/home/mgalloy/projects/mlso-utilities/bin/hpss
 
 TO_EMAIL=mgalloy@ucar.edu
 FROM_EMAIL=mgalloy@ucar.edu
 
-$PYTHON $HPSS /CORDYN | mail -s 'HPSS /CORDYN report' -r $FROM_EMAIL $TO_EMAIL
+HEADER="<html><body><pre>"
+
+REPORT=$($PYTHON $HPSS $ROOT)
+
+FOOTER="</pre></body></html>"
+
+HTML_REPORT="$HEADER\n$REPORT\n$FOOTER"
+
+echo -e "$HTML_REPORT" | mail -s "$(echo -e "HPSS report for $ROOT\nContent-Type: text/html")" -r $FROM_EMAIL $TO_EMAIL
