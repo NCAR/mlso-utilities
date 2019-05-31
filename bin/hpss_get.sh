1cf7afa75e4ed1e3098da877f40aee7c7eadda9e
Michael Galloy <mgalloy@gmail.com>
2019-05-31 10:32:11 -0600

    Make hpss_get.sh a full solution
    - specify output location
    - put full total size of files in comment

--- bin/hpss_get.sh
+++ bin/hpss_get.sh
@@ -1,22 +1,24 @@
 #!/bin/sh
 
-# need both action and instrument arguments to work
-if [ $# -lt 1 ]; then
-  echo "usage: $(basename $0) hpss_dir [output_file]"
+# need both hpss and local dir arguments to work
+if [ $# -lt 2 ]; then
+  echo "usage: $(basename $0) hpss_dir local_root [output_file]"
   echo
   echo "positional arguments:"
   echo "  hpss_dir      directory to download, i.e., /CORDYN/COMP/2018"
+  echo "  local_root    local directory to download files to"
   echo "  output_file   output file for hsi commands"
   exit 1
 fi
 
 HSI=/opt/local/bin/hsi
-DIR=$1
+HPSS_DIR=$1
+LOCAL_ROOT=$2
 
-if [ $# -lt 2 ]; then
+if [ $# -lt 3 ]; then
   OUTPUT=hsi_cmds
 else
-  OUTPUT=$2
+  OUTPUT=$3
 fi
 
 WORKING_DIR=$(mktemp -d)
@@ -24,18 +26,23 @@ WORKING_DIR=$(mktemp -d)
 #echo "Output in ${WORKING_DIR}"
 
 echo "Getting information from HPSS..."
-hsi -P ls -PR ${DIR} > ${WORKING_DIR}/hsi_output
+hsi -P ls -PR ${HPSS_DIR} > ${WORKING_DIR}/hsi_output
 
 echo "Extracting tape information..."
-cat ${WORKING_DIR}/hsi_output | awk 'BEGIN {FS="\t"}; {if($1=="FILE") print $6, "\t", $2}' > ${WORKING_DIR}/tokenized
+cat ${WORKING_DIR}/hsi_output | awk 'BEGIN {FS="[\t ]+"}; {if($1=="FILE") print $6,"\t",$2}' > ${WORKING_DIR}/tokenized
 
 echo "Sorting..."
 cat ${WORKING_DIR}/tokenized | sort > ${WORKING_DIR}/sorted
 
 echo "Generating hsi input file..."
-cat ${WORKING_DIR}/sorted | awk 'BEGIN {FS="\t"}; {print "cget" $2}' > ${WORKING_DIR}/hsi_cmds
+cat ${WORKING_DIR}/sorted | awk -v LOCAL_ROOT="${LOCAL_ROOT}" 'BEGIN {FS="[\t ]+"}; {print "cget", LOCAL_ROOT $2,":",$2}' > ${WORKING_DIR}/hsi_cmds
 
-cp ${WORKING_DIR}/hsi_cmds ${OUTPUT}
+cat ${WORKING_DIR}/hsi_output | awk 'BEGIN {FS="\t"}; {print $3}' > ${WORKING_DIR}/sizes
+TOTAL_SIZE=$(awk '{s+=$0} END {print s}' ${WORKING_DIR}/sizes)
+echo "Total size: ${TOTAL_SIZE} bytes"
+
+echo "# Total size: ${TOTAL_SIZE} bytes" > $OUTPUT
+cat ${WORKING_DIR}/hsi_cmds >> ${OUTPUT}
 echo "Output written to ${OUTPUT}"
 
 echo "Cleaning up..."
