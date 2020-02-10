120be1e8452430f207157ab811f88de063e9ccb3
Michael Galloy <mgalloy@gmail.com>
2020-02-10 11:28:26 -0700

    Add ability to check multiple root directories

--- bin/fix_noncordyn_files.sh
+++ bin/fix_noncordyn_files.sh
@@ -1,20 +1,25 @@
 #!/bin/sh
 
-if [[ ($# -lt 1) || ($# -gt 1) ]]; then
+if [[ ($# -lt 1) ]]; then
   echo "syntax: $0 rootdir"
   exit 1
 fi
 
-ROOT_DIR=$1
+dirs=( "$@" )
+for d in "${dirs[@]}"; do
+  ROOT_DIR=$d
+  echo "checking $d..."
 
-# find non-cordyn files
-find $ROOT_DIR ! -group cordyn -exec sh -c "echo Changing ownership of {}; chown :cordyn {}" \;
+  # find non-cordyn files
+  find $ROOT_DIR ! -group cordyn -exec sh -c "echo Changing ownership of {}; chown :cordyn {}" \;
 
-# find non group-readable files
-find $ROOT_DIR ! -perm -g+r -exec sh -c "echo Making {} group readable; chmod g+r {}" \;
+  # find non group-readable files
+  find $ROOT_DIR ! -perm -g+r -exec sh -c "echo Making {} group readable; chmod g+r {}" \;
 
-# find non group-writeable files
-find $ROOT_DIR ! -perm -g+w -exec sh -c "echo Making {} group writeable; chmod g+w {}" \;
+  # find non group-writeable files
+  find $ROOT_DIR ! -perm -g+w -exec sh -c "echo Making {} group writeable; chmod g+w {}" \;
 
-# find directories which are not group executable
-find $ROOT_DIR ! -perm -g+x -and -type d -exec sh -c "echo Making directory {} executable; chmod g+x {}" \;
+  # find directories which are not group executable
+  find $ROOT_DIR ! -perm -g+x -and -type d -exec sh -c "echo Making directory {} executable; chmod g+x {}" \;
+done
+
