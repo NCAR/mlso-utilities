8b6135a72edc58800f33c7dba499c1a741b4f129
Michael Galloy <mgalloy@gmail.com>
2020-12-17 13:47:56 -0700

    Adding ssh keys to backing up crontabs

--- bin/backup_crontabs.sh
+++ bin/backup_crontabs.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 
+SSH_KEY=/home/mgalloy/.ssh/id_rsa2
+
 backup_location=/home/mgalloy/data/crontabs
 
 day=$(date +"%d")
 
 machines=( compdata mlsodata kaula mahi dawn sunrise twilight sunset )
 for m in "${machines[@]}"; do
-  echo "checking $m..."
-  ssh $m "crontab -l" > ${backup_location}/$m-crontab-${day}.txt
+  #echo "checking $m..."
+  ssh -i ${SSH_KEY} $m "crontab -l" > ${backup_location}/$m-crontab-${day}.txt
 done
