21d015b47f80f66fe2305cc35d52835b47ce1a9a
Michael Galloy <mgalloy@gmail.com>
2020-12-18 08:24:13 -0700

    Getting gateway.sh script to work with new naming conventions

--- bin/gateway.sh
+++ bin/gateway.sh
@@ -3,12 +3,13 @@
 PYTHON=/home/mgalloy/anaconda3/bin/python
 
 # need both action and instrument arguments to work
-if [ $# -lt 2 ]; then
+if [ $# -lt 3 ]; then
   echo "usage: $(basename $0) action instrument"
   echo
   echo "positional arguments:"
   echo "  action      action to perform: start, stop, or restart"
-  echo "  instrument  instrument to watch for, e.g., KCor, CoMP, UCoMP, etc."
+  echo "  method      method, e.g., hpss, local, globus"
+  echo "  instrument  instrument to watch for, e.g., kcor, comp, ucomp, etc."
   exit 1
 fi
 
@@ -18,30 +19,29 @@ umask 0002
 
 BIN_DIR=$(dirname $0)
 ACTION=$1
-INSTRUMENT=$2
+METHOD=$2
+INSTRUMENT=$3
 
 
 kill_instrument () {
-  pkill -f "mlso-archiverd --method hpss $1"
+  pkill -f "mlso-archiverd --method $1 $2"
 }
 
 start_instrument () {
-  $PYTHON $BIN_DIR/mlso-archiverd --method hpss $1
+  $PYTHON $BIN_DIR/mlso-archiverd --method $1 $2
 }
 
 case "$ACTION" in
+  start)
+    start_instrument $METHOD $INSTRUMENT
+    ;;
 
-start)
-  start_instrument $INSTRUMENT
-  ;;
-
-stop)
-  kill_instrument $INSTRUMENT
-  ;;
-
-restart)
-  kill_instrument $INSTRUMENT
-  start_instrument $INSTRUMENT
-  ;;
+  stop)
+    kill_instrument $METHOD $INSTRUMENT
+    ;;
+  restart)
+    kill_instrument $METHOD $INSTRUMENT
+    start_instrument $METHOD $INSTRUMENT
+    ;;
 esac
 
