7555b38cf1146daeca98e646a33cd3f195834439
Michael Galloy <mgalloy@gmail.com>
2021-03-01 07:39:11 -0700

    Handle dusk differently for clock checking

--- bin/check_clocks.sh
+++ bin/check_clocks.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
 
-machines=( compdata mlsodata kaula mahi dawn sunrise twilight sunset kodiak.mlso.ucar.edu dusk.mlso.ucar.edu )
+machines=( compdata mlsodata kaula mahi dawn sunrise twilight sunset kodiak.mlso.ucar.edu )
 for m in "${machines[@]}"; do
   echo "checking $m..."
   ssh $m /home/mgalloy/bin/check_clock.sh
+done
+
+machines=( dusk.mlso.ucar.edu )
+for m in "${machines[@]}"; do
+  echo "checking $m..."
+  ssh $m /export/data1/home/mgalloy/bin/check_clock.sh
 done
