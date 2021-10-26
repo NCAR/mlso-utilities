1470a060771ed1dbe50aef09032fc62825645564
Michael Galloy <mgalloy@gmail.com>
2021-10-26 16:34:37 -0600

    Adding script to produce PDF for yesterday's observer log

--- bin/olog2pdf.sh
+++ bin/olog2pdf.sh
@@ -3,8 +3,16 @@
 OLOG_FILENAME=$1
 
 DATE=${OLOG_FILENAME:5:8}
-PS_FILEANME=mlso_observer_log.${DATE}.ps
+TEXT_FILENAME=mlso_observer_log.${DATE}.txt
+PS_FILENAME=mlso_observer_log.${DATE}.ps
 PDF_FILENAME=mlso_observer_log.${DATE}.pdf
 
-enscript -p ${PS_FILENAME} ${OLOG_FILENAME}
-ps2pdf ${PS_FILENMAE} ${PDF_FILENAME}
+echo "creating ${PDF_FILENAME}..."
+cp ${OLOG_FILENAME} ${TEXT_FILENAME}
+enscript_cmd="enscript --no-header --output ${PS_FILENAME} ${TEXT_FILENAME}"
+echo ${enscript_cmd}
+${enscript_cmd}
+ps2pdf_cmd="ps2pdf ${PS_FILENAME} ${PDF_FILENAME}"
+echo ${ps2pdf_cmd}
+${ps2pdf_cmd}
+rm -f ${TEXT_FILENAME} ${PS_FILENAME}
---
+++ bin/yesterday_olog2pdf.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+YEAR=$(date +"%Y" --date="-1 day")
+cd /hao/ftp/d5/mlso/log/observer/${YEAR}
+
+YESTERDAY_OLOG_FILENAME=$(date +"mlso.%Yd%j.olog" --date="-1 day")
+
+OLOG2PDF=/home/mgalloy/projects/mlso-utilities/bin/olog2pdf.sh
+
+${OLOG2PDF} ${YESTERDAY_OLOG_FILENAME}
