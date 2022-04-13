57f061c53d8bf492d7e17e2993ca7503bb0b191a
Michael Galloy <mgalloy@gmail.com>
2022-04-13 14:30:06 -0600

    Check resolution of DOIs for #1

---
+++ bin/mlso_check_dois.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+
+"""Resolve DOIs and check they are resolving correctly.
+"""
+
+import argparse
+import csv
+import sys
+
+import requests
+
+
+def check_dois(records):
+    has_error = False
+    for r in records:
+        doi = r["doi"].strip()
+        standard = r["standard"].strip()
+        print(f"checking resolving {doi}...")
+
+        r = requests.get(doi)
+
+        if len(r.history) > 1:
+            result = r.history[-1].url
+        else:
+            result = r.url
+
+        if standard == result:
+            print(f"  resolved: {standard}")
+        else:
+            print(f"  error: {result}, not {standard}")
+            has_error = True
+    return(has_error)
+
+
+def read_csvfile(filename):
+    with open(filename) as csvfile:
+        csv_reader = csv.reader(csvfile)
+        records = [{'doi': line[0], 'standard': line[1]} for line in csv_reader]
+    return(records)
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Check DOI resolution")
+
+    filename_help = "filename of CSV containing DOIs and standard URL"
+    parser.add_argument("doi_csv_filename", type=str, help=filename_help)
+
+    args = parser.parse_args()
+
+    has_error = check_dois(read_csvfile(args.doi_csv_filename))
+    sys.exit(1 if has_error else 0)
+
+
+if __name__ == "__main__":
+    main()
---
+++ bin/mlso_check_dois.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+PYTHON=/home/mgalloy/anaconda3/bin/python
+ROOT=$(dirname $0)
+DOIS=${ROOT}/$1
+OUTPUT_FILENAME=$(mktemp /tmp/doi-check.XXXXXXXXX)
+
+${PYTHON} ${ROOT}/mlso_check_dois.py ${DOIS} > ${OUTPUT_FILENAME}
+STATUS=$?
+
+echo "Sent from $0 ($(whoami)@$(hostname))" > ${OUTPUT_FILENAME}
+
+if [ $STATUS -eq 0 ]; then
+  STATUS_DESCRIPTION=success
+else
+  STATUS_DESCRIPTION=failure
+fi
+
+SUBJECT="MLSO DOI resolution check (${STATUS_DESCRIPTION})"
+
+cat ${OUTPUT_FILENAME} | mail -s "${SUBJECT}" mgalloy@ucar.edu
