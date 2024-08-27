d25f6cd3168bc8fe6d6a0bd80f472f39751a0220
Michael Galloy <mgalloy@gmail.com>
2024-08-27 10:29:48 -0600

    Removing obsolete machines from crontab backing up

--- bin/backup_crontabs.sh
+++ bin/backup_crontabs.sh
@@ -6,7 +6,7 @@ backup_location=/home/mgalloy/data/crontabs
 
 day=$(date +"%d")
 
-machines=( compdata mlsodata kaula mahi dawn sunrise twilight sunset )
+machines=( mahi dawn sunrise twilight sunset )
 for m in "${machines[@]}"; do
   #echo "checking $m..."
   ssh -i ${SSH_KEY} $m "crontab -l" > ${backup_location}/$m-crontab-${day}.txt
