9f1af207cfdb0ebc5d2b93ffe7101ac315a9118e
Michael Galloy <mgalloy@gmail.com>
2025-03-10 15:15:22 -0600

    Fix offset when reporting which server's clock is off, fix error message

--- bin/log_clocks.sh
+++ bin/log_clocks.sh
@@ -3,9 +3,13 @@
 LOG_FILENAME=/home/mgalloy/clock_data/clocks.log
 
 machines=( mahi dawn sunrise twilight sunset sundog1 sundog2 sundog3 nicole hazel kodiak.mlso.ucar.edu dusk.mlso.ucar.edu )
-times="$(date +%s)"
+times=""
+sep=""
 for m in "${machines[@]}"; do
-  times="$times, $(ssh -i /home/mgalloy/.ssh/id_rsa2 $m date +%s)"
+    times="$times$sep$(ssh -i /home/mgalloy/.ssh/id_rsa2 $m date +%s)"
+    if [ -z $sep ]; then
+	sep=", "
+    fi
 done
 
 echo "$times" >> $LOG_FILENAME
