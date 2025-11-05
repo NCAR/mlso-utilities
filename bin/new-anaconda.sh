89db1242e058968cedf8665957edea5c9f2b11d3
Michael Galloy <mgalloy@gmail.com>
2025-11-05 12:57:33 -0700

    Update anaconda version

--- bin/new-anaconda.sh
+++ bin/new-anaconda.sh
@@ -2,18 +2,29 @@
 
 # run on dawn
 
-URL=https://repo.anaconda.com/archive/Anaconda3-2024.10-1-Linux-x86_64.sh
+URL=https://repo.anaconda.com/archive/Anaconda3-2025.06-0-Linux-x86_64.sh
+#URL=https://repo.anaconda.com/archive/Anaconda3-2024.10-1-Linux-x86_64.sh
 #URL=https://repo.anaconda.com/archive/Anaconda3-2023.07-2-Linux-x86_64.sh
 #URL=https://repo.anaconda.com/archive/Anaconda3-2023.03-Linux-x86_64.sh
 #URL=https://repo.anaconda.com/archive/Anaconda3-2022.05-Linux-x86_64.sh
 PREFIX=${HOME}/anaconda3
 
-mv ${PREFIX} ${PREFIX}.old
+MAXV=2
 
-wget ${URL}
+for n in $(seq $((MAXV-1)) -1 1); do
+    if [ -d ${PREFIX}.$n ]; then
+	mv ${PREFIX}.$n ${PREFIX}.$((n+1))
+	echo mv ${PREFIX}.$n ${PREFIX}.$((n+1))	
+    fi
+done
+if [ -d ${PREFIX} ]; then
+    mv ${PREFIX} ${PREFIX}.1
+fi
+
+if [ ! -f $(basename ${URL}) ]; then
+    wget ${URL}
+fi
+
 bash $(basename ${URL}) -b -p ${PREFIX}
-
 conda install --file conda-requirements.txt
-
 pip install -r requirements.txt
-
