948eb5013bfc63375e2ab6feb6b8db3e078388db
Michael Galloy <mgalloy@gmail.com>
2026-04-26 07:01:24 -0600

    Handle .txt filename for observer log

--- bin/yesterday_olog2pdf.sh
+++ bin/yesterday_olog2pdf.sh
@@ -6,9 +6,14 @@ YEAR=$(date +"%Y" --date="-1 day")
 DIR=/hao/ftp/d5/mlso/log/observer/${YEAR}
 cd ${DIR}
 
+YESTERDAY_TXT_FILENAME=$(date +"mlso.%Yd%j.txt" --date="-1 day")
 YESTERDAY_OLOG_FILENAME=$(date +"mlso.%Yd%j.olog" --date="-1 day")
 
 OLOG2PDF=$(dirname $0)/olog2pdf.sh
+
+if [ -f "${YESTERDAY_TXT_FILENAME}" ]; then
+  mv "${YESTERDAY_TXT_FILENAME}" "${YESTERDAY_OLOG_FILENAME}"
+fi
 
 if [ -f "${YESTERDAY_OLOG_FILENAME}" ]; then
     ${OLOG2PDF} ${YESTERDAY_OLOG_FILENAME}
